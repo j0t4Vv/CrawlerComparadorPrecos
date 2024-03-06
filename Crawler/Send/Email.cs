a724c6ea3d4f29a576dabd7a78eb34df57399a38
João V. S. Oliveira <145166602+j0t4Vv@users.noreply.github.com>
2024-03-06 11:20:08 -0300

    Update Email.cs

--- Crawler/Send/Email.cs
+++ Crawler/Send/Email.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Mail;
 
@@ -6,8 +6,21 @@ namespace Crawler.Send
 {
     public class Email
     {
+        public static string SolicitarEmailDestino()
+        {
+            Console.WriteLine("Por favor, insira o endereço de email para receber o resultado: ");
+            return Console.ReadLine();
+        }
+
         public static void EnviarEmail(string emailDestino, string nomeProdutoMercadoLivre, string precoProdutoMercadoLivre, string nomeProdutoMagazineLuiza, string precoProdutoMagazineLuiza, string MelhorCompra, string UrlMelhorCompra)
         {
+            // Verificar se o endereço de e-mail de destino é nulo ou vazio
+            if (string.IsNullOrEmpty(emailDestino))
+            {
+                Console.WriteLine("O endereço de e-mail de destino é nulo ou vazio.");
+                return;
+            }
+
             // Configurações do servidor SMTP do Gmail
             string smtpServer = "smtp-mail.outlook.com"; // Servidor SMTP do Gmail
             int porta = 587; // Porta SMTP do Gmail para TLS/STARTTLS
@@ -22,13 +35,12 @@ namespace Crawler.Send
                 client.EnableSsl = true; // Habilitar SSL/TLS
 
                 // Construir mensagem de e-mail
-                MailMessage mensagem = new MailMessage(remetente, emailDestino) //Colocar email depois de "remetente"
+                MailMessage mensagem = new MailMessage(remetente, emailDestino)
                 {
                     Subject = "Resultado do Crawler de comparação de preços",
                     Body = $"Mercado Livre\n Produto: {nomeProdutoMercadoLivre}\n Preço: {precoProdutoMercadoLivre}\n" +
                            $"Magazine Luiza\n Produto: {nomeProdutoMagazineLuiza}\n Preço: {precoProdutoMagazineLuiza}\n" +
                            $"Melhor Compra\n {MelhorCompra} - {UrlMelhorCompra}"
-
                 };
 
                 // Enviar e-mail
